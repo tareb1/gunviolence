dbc8cb25b8f4d7de42eb451ec1ae3e344add79d2
Rebecca <tareb1@uw.edu>
2018-10-09 17:41:24 -0700

    added all files

---
+++ js/app.js
@@ -0,0 +1,92 @@
+/* add your script methods and logic here */
+
+'use strict';
+//centering the map when it loads
+var map = L.map('map-container').setView([39.50 , -98.35], 4);
+
+var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+
+layer.addTo(map);
+
+//creating layer and variables for the stats
+var Unknown = L.layerGroup([]);
+var male = L.layerGroup([]);
+var female = L.layerGroup([]);
+var stats = 0;
+var femalecount = 0;
+var malecount = 0;
+var unknowncount = 0;
+
+//listener that accounts for the statistics function being run everytime a layer is removed or added.
+map.addEventListener('overlayadd overlayremove', statsfunction);
+
+//loop to go through the data
+function setCircle(data){
+	for (var i = 0; i < data.length; i++) {
+		var lat = data[i].lat;
+		var lon = data[i].lng;
+
+		var locator = new L.circle([lat,lon]);
+
+	if(data[i].victim.gender == 'Male'){
+		var circle = new L.circleMarker([lat,lon],{color:"blue"});
+		circle.addTo(male);
+		circle.bindPopup(data[i].victim.name+": "+data[i].summary + "("+"link".link(data["sourceUrl"])+")");
+		malecount++;
+	} else if (data[i].victim.gender == 'Female'){
+		var circle = new L.circleMarker([lat,lon],{color:"red"});
+		circle.addTo(female);
+		circle.bindPopup(data[i].victim.name+": "+data[i].summary + "("+"link".link(data["sourceUrl"])+")");
+		femalecount++;
+
+	 } else{
+		var circle = new L.circleMarker([lat,lon],{color:"green"});
+		circle.addTo(Unknown);
+		circle.bindPopup(data[i].victim.name+": "+data[i].summary + "("+"link".link(data["sourceUrl"])+")");
+		unknowncount++;
+	}
+	stats++;
+}
+}
+//adds layers to the map
+male.addTo(map);
+female.addTo(map);
+Unknown.addTo(map);
+
+var myLayerGroups = {
+	'Unknown' : Unknown,
+	'Male': male,
+	'Female' : female,
+}
+
+L.control.layers(null, myLayerGroups).addTo(map);
+
+//stats function which calculates the statistics displayed
+
+function statsfunction(){
+	var m = 0;
+	var f = 0;
+	var u = 0;
+
+	if(map.hasLayer(female)){
+		f=Math.round((femalecount/stats)*100);
+	}
+	if(map.hasLayer(male)){
+		m=Math.round((malecount/stats)*100);
+	}
+	if(map.hasLayer(Unknown)){
+		u=Math.round((unknowncount/stats)*100);
+	}
+
+   $("#statsFemale").html(f);
+   $("#statsMale").html(m);
+   $("#statsUnknown").html(u);
+}
+
+//callback function to process the data
+$.getJSON('data/data.min.json').then(setCircle);
+
+//ajaxfunction
+$(document).ajaxComplete(function(){
+	statsfunction()
+});
---
+++ js/map.js
@@ -0,0 +1,124 @@
+// Wait until your document is ready
+$(function() {
+
+    // Function to draw your map
+    var drawMap = function() {
+      // Create map and set view
+      var map = L.map('map-container').setView([37.697948, -98.4842], 4);
+      // Create a tile layer variable using the appropriate url
+      var tileLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+      // Add the layer to your map
+      tileLayer.addTo(map);
+      // Execute your function to get data
+      getData(map);
+  };
+
+    var getData = function(map) {
+
+      // Execute an AJAX request to get the data in data/data.csv file
+      var data = $.get('data/data.csv', function (data, error) {
+      // Use the PapaParse library to parse the information returned by your request
+          var parsedData = Papa.parse(data, {
+              header: true
+          }).data;
+          // data parsed as strings; convert strings into integers
+          for (var i = 0; i < parsedData.length; i++) {
+              parsedData[i].killed = parseInt(parsedData[i].killed);
+              parsedData[i].injured = parseInt(parsedData[i].injured);
+          }
+          customBuild(parsedData, map);
+      });
+
+  };
+
+    // Loop through your data and add the appropriate layers and points
+    var customBuild = function(parsedData, map) {
+        
+      // Be sure to add each layer to the map
+      
+      // Arrays that hold the markers; get pushed to layergroups later
+      var moreKillings = []; // array for markers with more killings
+      var moreInjuries = []; // array for markers with more injuries
+
+      for (var i = 0; i < parsedData.length; i++) {
+          if (parsedData[i].killed > parsedData[i].injured) {
+              var mark = L.circleMarker([parsedData[i].lat, parsedData[i].lng], {radius: parsedData[i].killed, color: '#ff0000'});              
+              mark.bindPopup('There were ' + "<b>"+ parsedData[i].injured + "</b>" + ' people injured and ' + 
+                    "<span style='color:#ff0000'><b>" + parsedData[i].killed+ "</span></b>" + 
+                    ' killed in ' + parsedData[i].city + ', ' + parsedData[i].state + '.');
+              moreKillings.push(mark);
+          } else { // number of injured is greater than number of people killed
+              var mark = L.circleMarker([parsedData[i].lat, parsedData[i].lng], {radius: parsedData[i].injured, color: '#000000'});
+              mark.bindPopup('There were ' + "<b>"+ parsedData[i].injured + "</b>" + ' people injured and ' + 
+                    "<span style='color:#ff0000'><b>" + parsedData[i].killed+ "</span></b>" + 
+                    ' killed in ' + parsedData[i].city + ', ' + parsedData[i].state + '.');
+              moreInjuries.push(mark);
+          }
+      }
+      
+      var moreKilled = L.layerGroup(moreKillings);
+      var moreInjured = L.layerGroup(moreInjuries);
+      var overlayMaps = {
+          " More Killed": moreKilled,
+          " More Injured": moreInjured
+      };
+      
+      moreKilled.addTo(map);
+      moreInjured.addTo(map);
+      
+      // Once layers are on the map, add a leaflet controller that shows/hides layers
+      
+      L.control.layers(null, overlayMaps).addTo(map);
+      
+      // Build a table showing calculated aggregate values
+      var table = $('<table></table>');
+      table.addClass('table');
+  
+      // construct table headers
+      table.append('<th>State</th>');
+      table.append('<th>Injured</th>');
+      table.append('<th>Killed</th>');
+      
+      // initialize array of states, the number killed, and the number injured
+      var stateArray = [
+          {
+              state: parsedData[0].state,
+              killed: parsedData[0].killed,
+              injured: parsedData[0].injured
+          }
+      ];
+      
+      // loop through data, sum number of killed and injured for each state
+      for (var i = 1; i < parsedData.length; i++) {
+          var found = false;
+          for (var j = 0; j < stateArray.length; j++) {
+              if (stateArray[j].state == parsedData[i].state) {
+                  stateArray[j].killed += parsedData[i].killed;
+                  stateArray[j].injured += parsedData[i].injured;
+                  found = true;
+              } 
+          }
+          if (found == false) {
+              stateArray.push({
+                  state: parsedData[i].state,
+                  killed: parsedData[i].killed,
+                  injured: parsedData[i].injured
+              });
+          }
+      }
+      
+      // add states and their killed/injured numbers to the table
+      for (var i = 0; i < stateArray.length; i++) {
+          var tr = $('<tr>');
+          tr.append('<td><span style= opacity:.7>' + stateArray[i].state + '</td></span');
+          tr.append('<td><span style= opacity:.7>' + stateArray[i].injured + '</td></span');
+          tr.append('<td><span style= opacity:.7>' + stateArray[i].killed + '</td></span');
+          table.append(tr);
+      }
+      
+      $('#table-container').append(table);
+  };
+
+  // Execute your drawMap function
+  drawMap();
+});
